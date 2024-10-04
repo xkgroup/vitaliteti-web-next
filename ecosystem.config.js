09e36a716191954f6c5d301d5952d9934d10dcd4
Ubuntu <ubuntu@ip-172-26-7-153.eu-central-1.compute.internal>
2024-10-04 09:19:50 +0000

    added ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = { 
+        apps: [
+                { 
+                name: 'vitaliteti-web-next', // Your project name 
+                cwd: '/home/ubuntu/vitaliteti-web-next', // Path to your project 
+                script: 'npm', // For this example we're using npm, could also >
+                args: 'start'
+        }
+    ]
+};
