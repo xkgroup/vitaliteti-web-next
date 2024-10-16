0f12cf6d3a34ba22314402b2aed94614196003e2
veronosmani <veron.osmani@gmail.com>
2024-10-16 16:30:45 +0200

    modified:   src/app/search/components/content.js

--- src/app/search/components/content.js
+++ src/app/search/components/content.js
@@ -12,37 +12,6 @@ const Content = () => {
   const [inputText, setInputText] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const [hasSearched, setHasSearched] = useState(false); // New state to track search button click
-  const [newsListData] = useState([
-    {
-      img: NewsOne,
-      title:
-        "Gjërat që duhet të bëni nëse kaloni shumë kohë në këmbë gjatë ditës",
-      description:
-        "Shtypja e lartë e gjakut mund të çojë në një sërë komplikimesh të rrezikshme, duke përfshirë sëmundjet e zemrës dhe goditjen në tru, por jo gjithmonë ka simptoma të dukshme.",
-      date: "3 javë më parë",
-    },
-  ]);
-  const [newsList, setNewsList] = useState([]);
-
-  useEffect(() => {
-    // search filter for newsList
-    if (!searchQuery) {
-      setNewsList(newsListData);
-      return;
-    }
-
-    setNewsList(
-      newsListData.filter(
-        (prevNewsItem) =>
-          prevNewsItem.title
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase()) ||
-          prevNewsItem.description
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-      )
-    );
-  }, [searchQuery, newsListData]);
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
@@ -78,7 +47,7 @@ const Content = () => {
         <h1 className="search-section-one-result-text">Rezultatet për:</h1>
         <h1 className="search-section-one-result-text-input">{searchQuery}</h1>
       </div>
-      {hasSearched && newsList.length === 0 ? (
+      {hasSearched ? (
         <div className="search-section-two">
           <div className="search-section-two-row">
             <p className="search-section-two-row-text">Asnjë rezultatë...</p>
